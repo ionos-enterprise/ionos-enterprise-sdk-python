8ad1ddada7de92abfb81577b912d2cd38c361103
Frank Brehm <frank.brehm@profitbricks.com>
2016-02-18 18:08:12 +0100

    Extended description of methods in folder examples
    * update_image()
    * update_snapshot()

--- examples/snapshot_examples.py
+++ examples/snapshot_examples.py
@@ -15,13 +15,48 @@ for s in snapshots['items']:
 from profitbricks.client import ProfitBricksService
 
 snapshot_id = '7df81087-5835-41c6-a10b-3e098593bba4'
-# datacenter_id = '700e1cab-99b2-4c30-ba8c-1d273ddba022'
 
 client = ProfitBricksService(
     username='username', password='password')
 
 snapshot = client.get_snapshot(
     snapshot_id=snapshot_id)
+
+"""
+Update Snapshot
+
+Valid snapshot parameters are:
+
+* name (str)
+* description (str)
+* licence_type (one of 'LINUX', 'WINDOWS' or 'UNKNOWN')
+* cpu_hot_plug (bool)
+* ram_hot_plug (bool)
+* nic_hot_plug (bool)
+* nic_hot_unplug (bool)
+* disc_virtio_hot_plug (bool)
+* disc_virtio_hot_unplug (bool)
+* disc_scsi_hot_plug (bool)
+* disc_scsi_hot_unplug (bool)
+
+"""
+from profitbricks.client import ProfitBricksService
+
+client = ProfitBricksService(
+    username='username', password='password')
+
+snapshot_id = 'd084aa0a-f9ab-41d5-9316-18163bc416ef'
+image = client.update_snapshot(
+    snapshot_id,
+    name='New name',
+    description="Backup of volume XYZ",
+    licence_type='LINUX',
+    cpu_hot_plug=True,
+    ram_hot_plug=True,
+    nic_hot_plug=True,
+    nic_hot_unplug=True,
+    disc_virtio_hot_plug=True,
+    disc_virtio_hot_unplug=True)
 
 """Remove Snapshot
 """
