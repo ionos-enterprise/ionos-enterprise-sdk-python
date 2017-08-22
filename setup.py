413d8b75a719c6fac98b364135a9294e610c6fe5
Benjamin Drung <benjamin.drung@profitbricks.com>
2017-08-22 15:15:26 +0200

    Use UTF-8 as encoding for reading README.md
    README.md uses UTF-8 characters and Python will fail to open the file
    when using LC_ALL=posix:
    
    I: pybuild base:184: python3.6 setup.py clean
    Traceback (most recent call last):
      File "setup.py", line 48, in <module>
        long_desc = read('README.md')
      File "setup.py", line 35, in read
        return codecs.open(os.path.join(here, *parts), 'r').read()
      File "/usr/lib/python3.6/encodings/ascii.py", line 26, in decode
        return codecs.ascii_decode(input, self.errors)[0]
    UnicodeDecodeError: 'ascii' codec can't decode byte 0xe2 in position 11333: ordinal not in range(128)
    
    Therefore enforce opening README.md always with UTF-8 encoding.
    
    Bug-Debian: https://bugs.debian.org/872506

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 
 def read(*parts):
-    return codecs.open(os.path.join(here, *parts), 'r').read()
+    return codecs.open(os.path.join(here, *parts), 'r', 'utf-8').read()
 
 
 def find_version(*file_paths):
