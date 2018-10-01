d336b2b63bcb7fe5ec9497af14d94a69b9d276c7
Benjamin Drung <benjamin.drung@profitbricks.com>
2018-10-01 17:28:28 +0200

    Get rid of pointless statements
    pylint complains: Statement seems to have no effect
    (pointless-statement). These statements are just used to check whether
    the environment variable is defined.

--- tests/helpers/configuration.py
+++ tests/helpers/configuration.py
@@ -24,13 +24,11 @@ HEADERS = {'Connection': 'close'}
 
 # Import environment variables for credentials.
 try:
-    os.environ['PROFITBRICKS_USERNAME']
-    USERNAME = os.getenv('PROFITBRICKS_USERNAME')
+    USERNAME = os.environ['PROFITBRICKS_USERNAME']
 except KeyError:
     raise Exception('Please set the environment variable PROFITBRICKS_USERNAME')
 
 try:
-    os.environ['PROFITBRICKS_PASSWORD']
-    PASSWORD = os.getenv('PROFITBRICKS_PASSWORD')
+    PASSWORD = os.environ['PROFITBRICKS_PASSWORD']
 except KeyError:
     raise Exception('Please set the environment variable PROFITBRICKS_PASSWORD')
